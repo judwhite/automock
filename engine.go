4c0c9650b4ce47fa8794bbd6e5ad13802ca64d2a
Jud White <jud.white@blueskysystems.com>
2024-10-27 11:36:41 -0500

    engine: handle "go"

--- engine.go
+++ engine.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"os"
 	"sort"
@@ -9,6 +10,9 @@ import (
 	"sync"
 	"time"
 
+	"golang.org/x/xerrors"
+
+	"stockhuman/bitboard"
 	"stockhuman/lichess"
 )
 
@@ -150,7 +154,7 @@ func (e *Engine) ParseInput(line string) {
 	case "position":
 		e.handlePosition(line)
 	case "go":
-		uciWriteLine("info string 'go' TODO")
+		e.handleGo(line)
 	case "ponderhit":
 		uciWriteLine("info string 'ponderhit' TODO")
 	case "stop":
@@ -171,7 +175,7 @@ func (e *Engine) ParseInput(line string) {
 func (e *Engine) handleUCI() {
 	var sb strings.Builder
 	sb.WriteString(fmt.Sprintf("id name %s %s\n", EngineName, Version))
-	sb.WriteString(fmt.Sprintf("id name author %s\n", Author))
+	sb.WriteString(fmt.Sprintf("id author %s\n", Author))
 	for _, uciOption := range e.UCIOptions {
 		sb.WriteString(uciOption.String())
 		sb.WriteByte('\n')
@@ -190,8 +194,8 @@ func (e *Engine) handleIsReady() {
 }
 
 func (e *Engine) handleSetOption(line string) {
-	name, value := e.parseSetOption(line)
-	if name == "" {
+	name, value, err := parseSetOption(line)
+	if err != nil {
 		return
 	}
 
@@ -300,14 +304,14 @@ func (e *Engine) handleSetOption(line string) {
 	}
 }
 
-func (e *Engine) parseSetOption(line string) (string, string) {
+func parseSetOption(line string) (string, string, error) {
 	parts := strings.Split(line, " ")
 	// setoption name <name> value <value>
 	if len(parts) < 5 {
-		return "", ""
+		return "", "", xerrors.Errorf("setoption string has too few parameters: '%s'", line)
 	}
 	if parts[0] != "setoption" || parts[1] != "name" {
-		return "", ""
+		return "", "", xerrors.Errorf("setoption string does not start with 'setoption name': '%s'", line)
 	}
 
 	optionName := parts[2]
@@ -321,7 +325,7 @@ func (e *Engine) parseSetOption(line string) (string, string) {
 
 	i++
 	if i >= len(parts) {
-		return "", ""
+		return "", "", xerrors.Errorf("setoption string does not contain 'value': '%s'", line)
 	}
 
 	optionValue := strings.Join(parts[i:], " ")
@@ -330,7 +334,7 @@ func (e *Engine) parseSetOption(line string) (string, string) {
 		optionValue = ""
 	}
 
-	return optionName, optionValue
+	return optionName, optionValue, nil
 }
 
 func (e *Engine) handleShow() {
@@ -365,7 +369,7 @@ func (e *Engine) handlePosition(line string) {
 
 	i := 2
 	if parts[1] == "startpos" {
-		fen = lichess.StartPos
+		fen = bitboard.StartPos
 	} else if parts[1] == "fen" {
 		for ; i < len(parts); i++ {
 			if parts[i] == "moves" {
@@ -420,3 +424,237 @@ func (e *Engine) handleD() {
 
 	uciWriteLine(sb.String())
 }
+
+func (e *Engine) handleGo(line string) {
+	start := time.Now()
+
+	args, err := parseGo(line)
+	if err != nil {
+		uciWriteLine(fmt.Sprintf("info string %s", err.Error()))
+		return
+	}
+
+	_ = args
+	ctx := context.Background() // TODO: make this a cancellable context for 'stop'
+
+	fen, moves := e.readPosition()
+
+	speeds := e.LichessSpeeds
+	minRating := e.LichessRatingMin
+	maxRating := e.LichessRatingMax
+	since := e.LichessSince
+	until := e.LichessUntil
+
+	if int(minRating) > int(maxRating) {
+		minRating, maxRating = maxRating, minRating
+	}
+
+	var ratings lichess.Ratings
+	for _, item := range lichess.ValidRatings {
+		if item >= minRating && item <= maxRating {
+			ratings = append(ratings, item)
+		}
+	}
+
+	req := lichess.NewOpeningExplorerRequest()
+	req.FEN = fen
+	req.Play = strings.Join(moves, ",")
+	req.Speeds = speeds
+	req.Ratings = ratings
+	req.Since = since
+	req.Until = until
+
+	resp, err := lichess.GetLichessGames(ctx, req)
+	if err != nil {
+		panic(err)
+	}
+
+	uci := getSuggestedMove(resp)
+
+	ms := time.Since(start).Milliseconds()
+	msg := fmt.Sprintf("info depth 1 time %d nodes 1 score cp 0 nps 1 tbhits 0 pv %s\nbestmove %s\n", ms, uci, uci)
+	uciWriteLine(msg)
+}
+
+type GoArgs struct {
+	SearchMoves []string
+	Ponder      bool
+	WTime       int
+	BTime       int
+	WInc        int
+	BInc        int
+	MovesToGo   int
+	Depth       int
+	Nodes       int
+	Mate        int
+	MoveTime    int
+	Infinite    bool
+}
+
+func parseGo(line string) (GoArgs, error) {
+	parts := strings.Split(line, " ")
+	if len(parts) < 2 {
+		return GoArgs{}, xerrors.Errorf("go string has too few parameters: '%s'", line)
+	}
+
+	var goArgs GoArgs
+	i := 1
+
+	getInt := func(cmd string) (int, error) {
+		if i >= len(parts)-1 {
+			return 0, xerrors.Errorf("go string has missing argument after '%s': '%s'", cmd, line)
+		}
+		n, err := strconv.Atoi(parts[i+1])
+		if err != nil || n < 0 {
+			return 0, xerrors.Errorf("go string has invalid int argument after '%s': '%s'", cmd, line)
+		}
+		i += 2
+		return n, nil
+	}
+
+loop:
+	for i < len(parts) {
+		cmd := strings.ToLower(parts[i])
+		switch cmd {
+		case "searchmoves":
+			if i >= len(parts)-1 {
+				return GoArgs{}, xerrors.Errorf("go string has missing argument after '%s': '%s'", cmd, line)
+			}
+
+			goArgs.SearchMoves = parts[i+1:]
+			break loop
+		case "ponder":
+			goArgs.Ponder = true
+			i++
+		case "wtime":
+			if n, err := getInt(cmd); err != nil {
+				return goArgs, err
+			} else {
+				goArgs.WTime = n
+			}
+		case "btime":
+			if n, err := getInt(cmd); err != nil {
+				return goArgs, err
+			} else {
+				goArgs.BTime = n
+			}
+		case "winc":
+			if n, err := getInt(cmd); err != nil {
+				return goArgs, err
+			} else {
+				goArgs.WInc = n
+			}
+		case "binc":
+			if n, err := getInt(cmd); err != nil {
+				return goArgs, err
+			} else {
+				goArgs.BInc = n
+			}
+		case "movestogo":
+			if n, err := getInt(cmd); err != nil {
+				return goArgs, err
+			} else {
+				goArgs.MovesToGo = n
+			}
+		case "depth":
+			if n, err := getInt(cmd); err != nil {
+				return goArgs, err
+			} else {
+				goArgs.Depth = n
+			}
+		case "nodes":
+			if n, err := getInt(cmd); err != nil {
+				return goArgs, err
+			} else {
+				goArgs.Nodes = n
+			}
+		case "mate":
+			if n, err := getInt(cmd); err != nil {
+				return goArgs, err
+			} else {
+				goArgs.Mate = n
+			}
+		case "movetime":
+			if n, err := getInt(cmd); err != nil {
+				return goArgs, err
+			} else {
+				goArgs.MoveTime = n
+			}
+		case "infinite":
+			goArgs.Infinite = true
+			i++
+		default:
+			return GoArgs{}, xerrors.Errorf("go string has unrecognized command '%s' at position %d: '%s'", cmd, i, line)
+		}
+	}
+
+	return goArgs, nil
+}
+
+/*
+High Importance (Weight 9-10):
+
+Move aesthetics:
+- [ ] Sacrifices:
+  - [ ] Sacrifices a piece (bishop, knight, or rook for pawn)
+  - [ ] Hangs a piece
+  - [ ] Gambits a pawn
+  - [ ] Exchange sacrifice
+  - [ ] Promotion to knight or bishop with minimal impact to DTM
+- [ ] Attack:
+  - [ ] Exposes enemy King
+  - [ ] Pins a piece to opponent's king, queen, or rook
+  - [ ] Forks
+- [ ] Sharpness:
+  - [ ] Opponent is left with a position that has only 1 move. Excludes piece exchanges.
+  - [ ] Punishment of Natural-Looking Moves (recaptures, castling, capturing a hanging piece)
+- [ ] Imbalances:
+  - [ ] Trades a knight for a bishop or vice versa
+  - [ ] Unusual imbalances (e.g. Queen vs. Rook + Minor Piece, for either side)
+- [ ] Activity:
+  - [ ] Opens the center
+  - [ ] Piece activity (mobility)
+  - [ ] Pawn breaks
+- [ ] Positional:
+  - [ ] Opponent is left with isolated, doubled, or backward pawns
+  - [ ] Move results in passed pawn for "hero"
+  - [ ] We have an isolated pawn, but it's good or good enough.
+
+Move aesthetics (need help defining):
+- [ ] Piece coordination: Analyze connectivity between pieces (support and control of squares).
+- [ ] Quiet Moves in Tactics: Identify moves that are not captures or checks but improve evaluation.
+- [ ] Zugzwang Positions
+- [ ] Prophylactic Moves
+
+Specific Checkmate Patterns:
+- [ ] Perfect mate
+- [ ] Underpromotion mate (bishop or knight)
+- [ ] Double-check mate
+- [ ] Discovered check mate
+- [ ] Pawn mate
+- [ ] King mate
+- [ ] O-O#
+- [ ] O-O-O#
+- [ ] En Passant mate
+- [ ] Smothered Mate
+- [ ] Smothered Pork Mate, a smothered checkmate that involves a pinned piece and a fork. Example FEN: r3r1bk/1p2q1bp/p5N1/2p2p2/2BP1B2/4P2Q/PPP2PP1/2K4R b - - 1 24
+- [ ] Block a check with checkmate
+- [ ] Bishop + Knight checkmate
+- [ ] 2-Bishop checkmate
+- [ ] 4-Knight checkmate. Example FEN: 8/8/8/6k1/4nn2/3nn3/8/4K3 w - - 16 91
+- [ ] 4-Knight Cube Checkmate. Example FEN: 2K5/8/1nnk4/1nn5/8/8/8/8 w - - 21 77
+- [ ] 6-Knight Rectangle Checkmate. Example FEN: 6k1/8/4NNN1/4NNN1/8/8/1K6/8 b - - 40 104
+
+Game aesthetics:
+- [ ] Openings:
+  - [ ] Popular Openings
+  - [ ] Bizarre Openings
+  - [ ] Avoid "Mid-Level" Openings
+- [ ] Accuracy:
+  - [ ] "Hero" Never Significantly Behind. Threshold: -2.0.
+  - [ ] Efficient Conversion of Advantage: Avoid mid-game and end-game inaccuracies.
+  - [ ] Efficient Maneuvering (detect transpositions, choose shorter path)
+- [ ] Anti-Draw:
+  - [ ] Avoid repetitions and lines which force them
+  - [ ] Do not allow draw by repetition or perpetual check
+*/
