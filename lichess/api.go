359690333a521de6ec303684ea977eddba76edda
Jud White <jud.white@blueskysystems.com>
2024-10-28 12:01:07 -0500

    lichess: filter duplicates from cloud-eval

--- lichess/api.go
+++ lichess/api.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"net/url"
 	"strconv"
+	"strings"
 
 	"golang.org/x/xerrors"
 
@@ -57,6 +58,69 @@ func GetCloudEval(ctx context.Context, fen string, multiPV int) (CloudEvalRespon
 	var response CloudEvalResponse
 	if err := json.Unmarshal(b, &response); err != nil {
 		return CloudEvalResponse{}, xerrors.Errorf("%w", err)
+	}
+
+	// NOTE: Response has been seen with duplicates.
+	// URL: https://lichess.org/api/cloud-eval?fen=rnbqkb1r%2Fp1pppppp%2F1p3n2%2F8%2F8%2F1P3N2%2FPBPPPPPP%2FRN1QKB1R+b+KQkq+-+1+3&multiPv=3
+	// Response:
+	// {
+	//     "fen": "rnbqkb1r/p1pppppp/1p3n2/8/8/1P3N2/PBPPPPPP/RN1QKB1R b KQkq - 1 3",
+	//     "knodes": 50450,
+	//     "depth": 31,
+	//     "pvs": [
+	//         {
+	//             "moves": "d7d5 g2g3 g7g6 c2c4 e7e6 f1g2 f8g7 e1h1 e8h8 d2d4",
+	//             "cp": 16
+	//         },
+	//         {
+	//             "moves": "c7c5 c2c4 c8b7 g2g3 g7g6 f1g2 f8g7 e1h1 e8h8 d2d4",
+	//             "cp": 17
+	//         },
+	//         {
+	//             "moves": "c7c5 c2c4 c8b7 g2g3 g7g6 f1g2 f8g7 e1h1 e8h8 d2d4",
+	//             "cp": 17
+	//         },
+	//         {
+	//             "moves": "c8b7 g2g3 e7e6 c2c4 g7g6 f1g2 f8g7 e1h1 e8h8 d2d4",
+	//             "cp": 19
+	//         },
+	//         {
+	//             "moves": "c8b7 g2g3 e7e6 c2c4 g7g6 f1g2 f8g7 e1h1 e8h8 d2d4",
+	//             "cp": 19
+	//         },
+	//         {
+	//             "moves": "e7e6 g2g3 c8b7 c2c4 g7g6 f1g2 f8g7 e1h1 e8h8 d2d4",
+	//             "cp": 21
+	//         },
+	//         {
+	//             "moves": "e7e6 g2g3 c8b7 c2c4 g7g6 f1g2 f8g7 e1h1 e8h8 d2d4",
+	//             "cp": 21
+	//         },
+	//         {
+	//             "moves": "h7h6 c2c4 c8b7 e2e3 e7e6 b1c3 c7c5 g2g3 f8e7 f1g2",
+	//             "cp": 30
+	//         },
+	//         {
+	//             "moves": "h7h6 c2c4 c8b7 e2e3 e7e6 b1c3 c7c5 g2g3 f8e7 f1g2",
+	//             "cp": 30
+	//         }
+	//     ]
+	// }
+
+	// filter duplicates
+	seen := make(map[string]struct{})
+
+	for i := 0; i < len(response.PVs); i++ {
+		pv := response.PVs[i].MovesUCI
+
+		uci := strings.Split(pv, " ")[0]
+		if _, ok := seen[uci]; ok {
+			response.PVs = append(response.PVs[:i], response.PVs[i+1:]...)
+			i--
+			continue
+		}
+
+		seen[uci] = struct{}{}
 	}
 
 	return response, nil
